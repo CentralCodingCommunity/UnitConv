b73cc053add1960d2005f7e97dd456057b3445e0
Deathsbreed <nicolas.ortega.froysa@gmail.com>
2015-01-25 16:23:57 -0600

    Not using C anymore.

--- src/UnitConv.py
+++ src/UnitConv.py
@@ -1,6 +1,6 @@
 ##
 # UnitConv, a commandline unit converter.
-# Copyright (C) 2014  Nicolás A. Ortega
+# Copyright (C) 2014  Nicolas A. Ortega
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,5 +15,33 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##
+import getopt, sys
 
 version = "v0.1"
+
+def printUsage():
+	print "Usage:", sys.argv[0], "[option] [<measure_argument> <input><input_unit>:<output_unit>]"
+
+def printHelp():
+	printUsage()
+	print """Still working on this bit."""
+
+def main(argv):
+	try:
+		opts, args = getopt.getopt(argv, "ht:v", ["help", "temp=", "version"])
+	except getopt.GetoptError:
+		printUsage()
+		sys.exit(2)
+
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			printHelp()
+			sys.exit(0)
+		elif opt in ("-v", "--version"):
+			print "UnitConv", version
+			sys.exit(0)
+		elif opt in ("-v", "--temp"):
+			print "You chose temparature!!!"
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
