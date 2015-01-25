1a61814dc9e6bbdfe0d78f8761c9635d56d3a1ea
Deathsbreed <nicolas.ortega.froysa@gmail.com>
2015-01-25 17:00:03 -0600

    Added conversion functions.

--- src/UnitConv.py
+++ src/UnitConv.py
@@ -20,18 +20,61 @@ import getopt, sys
 # Version tag
 version = "v0.1"
 
+# What measurement are we converting in
+measurement = ""
+
+# Input and output units
+iunit = ""
+ounit = ""
+
 ### PREDEFINED FUNCTIONS ###
 def printUsage():
 	print "Usage:", sys.argv[0], "[option] | [<measurement> <input><input_unit>:<output_unit>]"
 
 def printHelp():
 	printUsage()
-	print """Still working on this bit."""
+	print """
+Measurements and Units:
+	-a or --area		-- Area
+	-d or --dist		-- Distance
+	-m or --mass		-- Mass
+	-t or --temp		-- Temperature
+		C		-- Celsius
+		F		-- Fahrenheit
+		K		-- Kelvin
+	-s or --speed		-- Speed
+	-v or --vol		-- Volume
+
+Other Arguments:
+	-h or --help		-- Print this help information.
+	--version		-- Print the version of UnitConv installed.
+
+Example:
+	""", sys.argv[0], """ --temp 32F:C"""
+
+### CONVERSION FUNCTIONS ###
+def areaConv(arg):
+	print "Area..."
+
+def distConv(arg):
+	print "Distance..."
+
+def massConv(arg):
+	print "Mass..."
+
+def tempConv(arg):
+	print "You chose temperature with argument", arg
+
+def speedConv(arg):
+	print "Speed..."
+
+def volConv(arg):
+	print "Volume..."
 
 ### MAIN FUNCTION ###
 def main(argv):
 	try:
-		opts, args = getopt.getopt(argv, "ht:v", ["help", "temp=", "version"])
+		opts, args = getopt.getopt(argv, "a:d:m:ht:s:v:", ["area=", "dist=", "help", "mass=", "temp=", "speed=", "version"])
 	except getopt.GetoptError:
 		printUsage()
 		sys.exit(2)
@@ -39,12 +82,22 @@ def main(argv):
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
 			printHelp()
-			sys.exit(0)
-		elif opt in ("-v", "--version"):
+		elif opt == "--version":
 			print "UnitConv", version
-			sys.exit(0)
+		elif opt in ("-a", "--area"):
+			areaConv(arg)
+		elif opt in ("-d", "--dist"):
+			distConv(arg)
+		elif opt in ("-m", "--mass"):
+			massConv(arg)
 		elif opt in ("-t", "--temp"):
-			print "You chose temparature!!!"
+			tempConv(arg)
+		elif opt in ("-s", "--speed"):
+			speedConv(arg)
+		elif opt in ("-v", "--vol"):
+			volConv(arg)
+
+		sys.exit(0)
 
 ### CALL MAIN FUNCTION ###
 if __name__ == "__main__":
